445eb9c8bcdc3a886de6b28862101693954e5971
athlon <athlon@debian1>
2020-03-13 03:20:55 +0200

    Initial commit

---
+++ jenkins_container_start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker container run --name jenkins-blueocean --rm --detach \
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  --publish 8080:8080 --publish 50000:50000 jenkinsci/blueocean
